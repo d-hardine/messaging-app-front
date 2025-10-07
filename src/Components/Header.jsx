91d1d4d9f1424ab387915e8a153c78df329bffcd
Dhiva Ryan H <176643369+d-hardine@users.noreply.github.com>
2025-10-07 12:54:13 +0700

    Initial react commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <div className="header-left-container">
+                asdaldkjasd
+            </div>
+            <div className="header-right-container">
+                askdhjaskdhl
+            </div>
+        </header>
+    )
+}
